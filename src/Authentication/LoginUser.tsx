bc985ebffe0fefeae02ae78793bf5235974edccc
Kamen Vakavchiev <kamen.vakavchiev@ocado.com>
2023-05-23 17:44:53 +0300

    fix login

--- src/Authentication/LoginUser.tsx
+++ src/Authentication/LoginUser.tsx
@@ -1,11 +1,14 @@
 import { FormEvent, useState } from "react";
 import { login } from "./AuthenticationService";
+import { useNavigate } from "react-router-dom";
 
 export function LoginUser() {
+    const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const handleSubmit = (event: FormEvent) => {
         login(username,password);
+        navigate("/");
     };
     return (
         <form onSubmit={handleSubmit}>
